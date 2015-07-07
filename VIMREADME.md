f07f9ef9c44b994797f283b274a86823545325ea
rooid <vsinhsawant@gmail.com>
2015-07-07 16:25:16 -0400

    added tabular plugin

--- VIMREADME.md
+++ VIMREADME.md
@@ -5,10 +5,31 @@
 1. Add the git url install.vimplugin.sh && execute
 
 
+### vim :help
 
+:help internal-variables <b,g,w>
+
+### Surround.vim
 To add head tag aroung the text object
 yss<head>
 
 To add body around a paragraph, 
 Shift+V S<body>
+
+In vimconsole to get the ascii type
+:echo char2nr("_")
+
+* dw : delete : verb: command ,w : noun : motion/text object
+* Macros + Register
+* Leader
+
+### ctrl-p
+
+<c-p> : <ctrl-p>
+<c-j>,
+    Move selection down.
+<c-k>,
+ Move selection up.
+
+
 
--- install.vimplugin.sh
+++ install.vimplugin.sh
@@ -11,15 +11,21 @@
 
 #Check the updated repository
 readonly bundledir=${PWD}/bundle
+IS_FORCE_INSTALL=false
 
-repoList=( 'git://github.com/scrooloose/nerdtree.git' 
+OldrepoList=( 'git://github.com/scrooloose/nerdtree.git' 
            'git://github.com/kien/ctrlp.vim.git'
            'git://github.com/tpope/vim-surround.git'
+           'git://github.com/kien/ctrlp.vim'
+           'git://github.com/godlygeek/tabular.git'
          )
 
+newRepoList=(  
+           'git://github.com/godlygeek/tabular.git'
+           )
 
-force_install() {
-    for repo in ${repoList[@]}
+install() {
+    for repo in ${newRepoList[@]}
     do
         echo "repo : ${repo}"
         dest="$bundledir/$(basename ${repo} | sed -e 's/\.git$//')"
@@ -30,4 +36,20 @@ force_install() {
     done
 }
 
-force_install
+force_install() {
+    install
+    for repo in ${OldrepoList[@]}
+    do
+        echo "repo : ${repo}"
+        dest="$bundledir/$(basename ${repo} | sed -e 's/\.git$//')"
+        rm -rf $dest
+        echo "Cloning $repo into $dest"
+        git clone -q $repo $dest
+        rm -rf $dest/.git
+    done
+}
+
+install
+
+${IS_FORCE_INSTALL} && force_install 
+
