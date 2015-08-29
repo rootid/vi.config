125eced6e465e946db1aea961c43ab2dd4167914
rooid <vsinhsawant@gmail.com>
2015-08-29 03:44:43 -0400

    updated dev

--- tmux_dev.sh
+++ tmux_dev.sh
@@ -1,7 +1,8 @@
 #After installing tmux 1.8 dev environment invoke the script
 
+DIR="~/code/"
 tmux new-session -s dev -n ide -d
-tmux send-keys -t dev 'cd ~/code/' C-m
+tmux send-keys -t dev 'cd $DIR' C-m
 tmux send-keys -t dev 'vim .' C-m
 tmux new-window -n shell
 tmux select-window -t dev:1
